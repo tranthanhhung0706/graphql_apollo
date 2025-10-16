4dd18f6a4ade8ba341e558be545f5f839991fcfc
hung <hungdangcap99@gmail.com>
2025-10-17 00:44:52 +0700

    add nodemon

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-test
+
+const express = require('express');
+ const expressGraphQL = require('express-graphql').graphqlHTTP;
+const app = express();
+const PORT = 4000;
+
+app.use('/graphql',expressGraphQL({
+    graphiql:true
+}))
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
